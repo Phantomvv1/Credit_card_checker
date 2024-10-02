8a49911d7249a2bbee54a508cfc978523b6da656
Phantomvv1 <krisi_andreev@abv.bg>
2024-10-02 09:03:38 +0300

    Made a working Luhn algorithm
    Created a Luhn's algorithm in order to check if the credit card number
    is valid

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sumDigits(number int) int {
+	if number <= 9 {
+		return number
+	} else {
+		return number/10 + number%10
+	}
+}
+
+func luhnAlogorithm(number int) bool {
+	checkDigit := number % 10
+	number = number / 10
+	var arrToSum []int
+	for i := 0; number > 0; i++ {
+		if i%2 == 1 {
+			arrToSum = append(arrToSum, number%10)
+		} else {
+			arrToSum = append(arrToSum, (number%10)*2)
+		}
+		number = number / 10
+	}
+	for i, number := range arrToSum {
+		arrToSum[i] = sumDigits(number)
+	}
+	var result int
+	for _, num := range arrToSum {
+		result = result + num
+	}
+	result = 10 - result%10
+	return result == checkDigit
+}
+
+func main() {
+	fmt.Println(luhnAlogorithm(17893729974))
+}
